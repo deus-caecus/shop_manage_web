3272aee44dfb890d9b7c771edd063c5126e579b1
1799817296 <1799817296@qq.com>
2024-12-08 12:14:49 +0800

    饿了么中文

--- demo_vue/src/main.js
+++ demo_vue/src/main.js
@@ -2,10 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+
 app.use(router)
 app.mount('#app')
