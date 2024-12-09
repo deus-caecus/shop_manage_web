68dc91920058dc7e50a8880c4dc55e9e8931c1cc
xwz <1739553676@qq.com>
2024-12-09 16:50:50 +0800

    supplier 接口

--- demo_vue/src/api/SupplyChain/SupplierManagement.js
+++ demo_vue/src/api/SupplyChain/SupplierManagement.js
@@ -1,1 +1,41 @@
-import {}
+import interceptor from '@/utils/interceptor'
+
+export const supplierQueryService = () => {
+  return interceptor
+    .get('/supplier'/*url*/)
+    .then(response => {
+      return response.data
+    }).catch(error => {
+      console.log(error)
+    })
+}
+
+export const supplierAddService = (newSupplier) => {
+  return interceptor
+  .post('/supplier/add'/*  */,newSupplier)
+  .then(response=>{
+    return response.data
+  }).catch(error=>{
+    console.log(error)
+  })
+}
+
+export const supplierDeleteService = (supplierID) =>{
+  return interceptor
+  .delete(`/delete/${supplierID}`)
+  .then(response=>{
+    return response.data
+  }).catch(error=>{
+    console.log(error)
+  })
+}
+
+export const supplierUpdateService = (modifiledSupplier) =>{
+  return interceptor
+  .put('/supplier/update',modifiledSupplier)
+  .then(response=>{
+    return response.data
+  }).catch(error=>{
+    console.log(error)
+  })
+}
